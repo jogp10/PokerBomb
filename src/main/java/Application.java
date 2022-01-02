f1af008417c97423faf363b867f9837838b20142
Afonso Jorge Farroco Martins <up202005900@fe.up.pt>
2022-01-02 15:48:59 +0000

    Menu class

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -7,16 +7,7 @@ import java.io.IOException;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-
-        try {
-            Terminal terminal = new DefaultTerminalFactory().createTerminal();
-            Screen screen = new TerminalScreen(terminal);
-            screen.setCursorPosition(null); // we don't need a cursor
-            screen.startScreen(); // screens must be started
-            screen.doResizeIfNecessary(); // resize screen if necessary
-        }catch (IOException e){
-            e.printStackTrace();
-        }
+        Menu menu = new Menu();
+       //menu.mainMenu();
     }
 }
