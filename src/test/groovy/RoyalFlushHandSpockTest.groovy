e74e176a28dbf7a26df819c931e21f6b02306980
AlexSa1000 <diogoalexandre.costaesa@gmail.com>
2022-01-03 16:12:54 +0000

    Added tests to verify Royal Flush hands

---
+++ src/test/groovy/RoyalFlushHandSpockTest.groovy
@@ -0,0 +1,66 @@
+import spock.lang.Specification
+
+class RoyalFlushHandSpockTest extends Specification {
+    def 'Royal Flush Hand 1'() {
+        given:
+        Card card1 = new Card(Suit.HEART, Rank.TEN)
+        Card card2 = new Card(Suit.HEART, Rank.JACK)
+        Card card3 = new Card(Suit.HEART, Rank.QUEEN)
+        Card card4 = new Card(Suit.HEART, Rank.KING)
+        Card card5 = new Card(Suit.HEART, Rank.ACE)
+        Deck deck = new Deck()
+        deck.addCard(card1)
+        deck.addCard(card2)
+        deck.addCard(card3)
+        deck.addCard(card4)
+        deck.addCard(card5)
+
+        when:
+        boolean royalFlush = Hand.isRoyalFlush(deck)
+
+        then:
+        royalFlush
+    }
+
+    def 'Royal Flush Hand 2'() {
+        given:
+        Card card1 = new Card(Suit.SPADE, Rank.TEN)
+        Card card2 = new Card(Suit.SPADE, Rank.JACK)
+        Card card3 = new Card(Suit.SPADE, Rank.QUEEN)
+        Card card4 = new Card(Suit.SPADE, Rank.KING)
+        Card card5 = new Card(Suit.SPADE, Rank.ACE)
+        Deck deck = new Deck()
+        deck.addCard(card1)
+        deck.addCard(card2)
+        deck.addCard(card3)
+        deck.addCard(card4)
+        deck.addCard(card5)
+
+        when:
+        boolean royalFlush = Hand.isRoyalFlush(deck)
+
+        then:
+        royalFlush
+    }
+
+    def 'Royal Flush Hand 3'() {
+        given:
+        Card card1 = new Card(Suit.DIAMOND, Rank.TEN)
+        Card card2 = new Card(Suit.DIAMOND, Rank.JACK)
+        Card card3 = new Card(Suit.DIAMOND, Rank.QUEEN)
+        Card card4 = new Card(Suit.DIAMOND, Rank.KING)
+        Card card5 = new Card(Suit.DIAMOND, Rank.ACE)
+        Deck deck = new Deck()
+        deck.addCard(card1)
+        deck.addCard(card2)
+        deck.addCard(card3)
+        deck.addCard(card4)
+        deck.addCard(card5)
+
+        when:
+        boolean royalFlush = Hand.isRoyalFlush(deck)
+
+        then:
+        royalFlush
+    }
+}
