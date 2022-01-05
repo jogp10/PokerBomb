7fe1bded305e4dd52f546db9d250ea255c41d656
jogp10 <jogp10@gmail.com>
2022-01-05 19:13:51 +0000

    Drawing cards

--- src/main/java/com/pockerbomb/view/cards/DynamiteCardView.java
+++ src/main/java/com/pockerbomb/view/cards/DynamiteCardView.java
@@ -1,8 +1,27 @@
 package com.pockerbomb.view.cards;
 
-public class DynamiteCardView extends CardView implements GenericSpecialCardView {
-    @Override
-    public void draw() {
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.pockerbomb.model.game.cards.Card;
+import com.pockerbomb.model.game.cards.DynamiteCard;
+import com.pockerbomb.view.View;
 
+import java.io.IOException;
+
+public class DynamiteCardView extends View<DynamiteCard> {
+    CardView cardView;
+
+    public DynamiteCardView(DynamiteCard model, TextGraphics graphics) {
+        super(model);
+        this.graphics=graphics;
+        cardView = new CardView(model, graphics);
+    }
+
+    @Override
+    public void draw(int col, int row) throws IOException {
+            cardView.draw(col, row);
+
+            graphics.setForegroundColor(TextColor.Factory.fromString("#FF0000"));
+            graphics.putString(col+2, row+4, String.valueOf(model.getSpecialAttribute()));
     }
 }
