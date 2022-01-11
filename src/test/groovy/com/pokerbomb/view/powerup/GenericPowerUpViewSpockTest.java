4ee5423dbc87d228d03002f485bca40180fb43f7
jogp10 <jogp10@gmail.com>
2022-01-11 20:51:43 +0000

    PowerUps testing

--- src/test/groovy/com/pokerbomb/view/powerup/GenericPowerUpViewSpockTest.java
+++ src/test/groovy/com/pokerbomb/view/powerup/GenericPowerUpViewSpockTest.java
@@ -4,9 +4,13 @@ import com.googlecode.lanterna.graphics.TextGraphics;
 import com.pokerbomb.model.game.powerup.GenericPowerUp;
 import com.pokerbomb.view.ViewTest;
 import org.mockito.Mock;
+import spock.lang.Specification;
 
 import java.io.IOException;
 
 public class GenericPowerUpViewSpockTest extends ViewTest<GenericPowerUp> {
-    TextGraphics graphics;
+
+    def setup() {
+
+    }
 }
