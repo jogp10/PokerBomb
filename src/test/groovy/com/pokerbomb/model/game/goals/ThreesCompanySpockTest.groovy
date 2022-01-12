ff305e27116a22b1b87867ba01e0d01f7952aade
AlexSa1000 <diogoalexandre.costaesa@gmail.com>
2022-01-12 19:28:37 +0000

    Added test to verify "three of a kind" goal

---
+++ src/test/groovy/com/pokerbomb/model/game/goals/ThreesCompanySpockTest.groovy
@@ -0,0 +1,33 @@
+package com.pokerbomb.model.game.goals
+
+import com.pokerbomb.model.game.cards.Card
+import com.pokerbomb.model.game.cards.Hand
+import com.pokerbomb.model.game.cards.Rank
+import com.pokerbomb.model.game.cards.Suit
+import spock.lang.Specification
+
+class ThreesCompanySpockTest extends Specification {
+    def "Three's Company com.pockerbomb.model.game.goals"() {
+        given:
+        ArrayList<Hand> hands = new ArrayList<>()
+
+        Card card1 = new Card(Suit.HEART, Rank.QUEEN)
+        Card card2 = new Card(Suit.SPADE, Rank.QUEEN)
+        Card card3 = new Card(Suit.CLUB, Rank.QUEEN)
+        Hand hand1 = new Hand()
+        hand1.addCard(card1)
+        hand1.addCard(card2)
+        hand1.addCard(card3)
+        hands.add(hand1)
+
+        ThreesCompanyGoal goal = new ThreesCompanyGoal(Type.THREE_OF_A_KIND, 1)
+
+        when:
+        for (Hand hand : hands) {
+            goal.increment(hand)
+        }
+
+        then:
+        goal.isAchieved()
+    }
+}
