61d7efe1b599fca1b9d3436124962dc6a7e3f604
AlexSa1000 <diogoalexandre.costaesa@gmail.com>
2022-01-16 23:37:52 +0000

    Added statements for different kind of goals

--- src/main/java/com/pokerbomb/view/game/goals/GoalViewFactory.java
+++ src/main/java/com/pokerbomb/view/game/goals/GoalViewFactory.java
@@ -8,8 +8,14 @@ public class GoalViewFactory {
         if(goal instanceof InDaClubGoal) {
             return new InDaClubGoalView((InDaClubGoal) goal, graphics);
         }
-        if(goal instanceof GiveUsAHandGoal) {
-            return new GiveUsAHandGoalView((GiveUsAHandGoal) goal, graphics);
+        if(goal instanceof DiamondMinerGoal) {
+            return new DiamondMinerGoalView((DiamondMinerGoal) goal, graphics);
+        }
+        if(goal instanceof HeartbreakerGoal) {
+            return new HeartbreakerGoalView((HeartbreakerGoal) goal, graphics);
+        }
+        if(goal instanceof CanYouDigItGoal) {
+            return new CanYouDigItGoalView((CanYouDigItGoal) goal, graphics);
         }
         if(goal instanceof CardCollectorGoal) {
             return new CardCollectorGoalView((CardCollectorGoal) goal, graphics);
@@ -23,8 +29,17 @@ public class GoalViewFactory {
         if(goal instanceof ThreesCompanyGoal) {
             return new ThreesCompanyGoalView((ThreesCompanyGoal) goal, graphics);
         }
+        if(goal instanceof SquadsOfQuadsGoal) {
+            return new SquadsOfQuadsGoalView((SquadsOfQuadsGoal) goal, graphics);
+        }
+        if(goal instanceof FlushedOutGoal) {
+            return new FlushedOutGoalView((FlushedOutGoal) goal, graphics);
+        }
         if(goal instanceof TheHouseIsFullGoal) {
             return new TheHouseIsFullGoalView((TheHouseIsFullGoal) goal, graphics);
+        }
+        if(goal instanceof RunRabbitRunGoal) {
+            return new RunRabbitRunGoalView((RunRabbitRunGoal) goal, graphics);
         }
         return null;
     }
