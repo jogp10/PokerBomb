832110b19f6793ddd072a53f7a8ea21c8c3c1e76
AlexSa1000 <diogoalexandre.costaesa@gmail.com>
2022-01-16 23:34:49 +0000

    Added view classes for different kind of goals

---
+++ src/main/java/com/pokerbomb/view/game/goals/DiamondMinerGoalView.java
@@ -0,0 +1,18 @@
+package com.pokerbomb.view.game.goals;
+
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.pokerbomb.model.game.goals.Goal;
+
+public class DiamondMinerGoalView extends GenericGoalView {
+
+    public DiamondMinerGoalView(Goal model, TextGraphics graphics) {
+        super(model, graphics);
+    }
+
+    @Override
+    public void draw(int col, int row) {
+        //graphics.setForegroundColor(TextColor.Factory.fromString("#FF0000"));
+        graphics.putString(col, row, "DIAMONDS");
+        graphics.putString(col, row+1, String.format("%1$s / %2$s", model.getCurrentQuantity(), model.getTotalQuantity()));
+    }
+}
