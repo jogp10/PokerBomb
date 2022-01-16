7a772356fa2ab33c1e8d27d9a3ade808264e9f0c
AlexSa1000 <diogoalexandre.costaesa@gmail.com>
2022-01-16 23:38:41 +0000

    Added types for other kind of goals

--- src/main/java/com/pokerbomb/model/game/goals/Type.java
+++ src/main/java/com/pokerbomb/model/game/goals/Type.java
@@ -2,11 +2,17 @@ package com.pokerbomb.model.game.goals;
 
 public enum Type {
     CLUBS,
+    DIAMONDS,
+    HEARTS,
+    SPADES,
     HANDS,
     CARDS,
     PAIR,
     TWO_PAIRS,
     THREE_OF_A_KIND,
+    FOUR_OF_A_KIND,
+    FLUSH,
     FULL_HOUSE,
+    STRAIGHT,
     SCORE
 }
